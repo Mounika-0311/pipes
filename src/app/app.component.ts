604511e536d54fb804519cb1b7771ecfb4ff1ffa
mounika <mounika.mupparaju@archents.com>
2023-10-03 15:16:41 +0530

    pipes concept

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipesTask';
+
+   //normal pipe
+   title1= "normal pipe";
+
+   Amount = 2000.122;
+
+   percentagedata = '0.86';
+
+   jsonObject = {"name": "mouni"}
+
+   sdgrate = 55.58
+
+   currentDate= new Date();
+  
+   filteredString : string = '';
+  users = [{
+      name : 'mounika',
+      city: 'Guntur',
+      age : '22'
+    },
+    {
+      name : 'saritha',
+      city: 'Hyderabad',
+      age : '25'
+    }
+  ]
+
+   appStatus=new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+      resolve('user data received');
+    },3000);
+   });
+
+   customdata = {
+    name : 'Archents',
+    city: 'vizag',
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustompipesPipe } from './pipe/custompipe.pipe';
+import { FilterpipePipe } from './pipe/filterpipe.pipe';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustompipesPipe,
+    FilterpipePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
